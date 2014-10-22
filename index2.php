c311d86b2f168e000a2229ce6b4dd7088969be3b
Neospiro <Neospiro@users.noreply.github.com>
2014-10-22 13:25:00 +0200

    Create index2.php

---
+++ index2.php
@@ -0,0 +1,45 @@
+<form action="" method="post">
+	<label>string:<input type="text" placeholder="Ecrivez n'importe quoi" name='string'></label><br/>
+	<label>action:valid<input hidden name="action" value='valid'></label><br/>
+	<input type="submit">
+</form>
+<form action="" method="post">
+	<label>action:unknown_func<input hidden name="action" value='unknown_func'></label><br/>
+	<input type="submit">
+</form>
+<?php
+//On inclue le SUPER SIMPLE SERVER
+include "sss/sss.php";
+
+function json_dump($var){
+	echo "<pre>".json_encode($var,JSON_PRETTY_PRINT )."</pre>";
+}
+
+//on instancie le serveur
+$server = new sss();
+
+//On définie une function "valid"
+$server->setAction("valid",function($args){
+	echo "L'utilisateur a ecrit ".$args["string"];
+	if($args["string"]		=="n'importe quoi"){
+		return "c'est le bordel";
+	}
+	else if($args["string"]	==""){
+		return "c'est pas du tout bon";
+	}
+	else return 'tout est ok';
+});
+
+
+
+//Si l'utilisateur DEMANDE une action, on peut alors l'utiliser
+if(isset($_POST["action"]))
+	try{
+		json_dump(
+			$server->callAction($_POST["action"], $_POST)
+		);
+	}
+	catch (Exception $e)
+	{
+		echo 'Erreur : '.$e->getMessage();
+	}
